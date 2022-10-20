8085b5e8b0d7768e0f37e272af45fb377e42258f
nivanjana <nivanjana@qburst.com>
2022-10-20 14:25:19 +0530

    added p4 question as comment

--- 4.py
+++ 4.py
@@ -3,10 +3,3 @@ l = []
 for i in range(n):
     l.append(int(input()))
 
-res = max(l)
-curMin, curMax = 1, 1
-
-for i in l:
-    m = max(i,m*i,m)
-    l
-
--- 4a.py
+++ 4a.py
@@ -1,3 +1,45 @@
+'''
+
+There’s a game in Squid Games where all the participants are given a roll of coins. Each coin will have a value (positive, negative, or zero). The participants can select any number of adjacent coins from the roll. Those who get the highest product value get to the next level. If you are participating in the game, can you pick the set of adjacent coins so that the product is the highest value?
+Examples:
+Roll contains coins: 6, -3, -10, 0, 2
+Highest product: 180  (The coins are 6, -3, and -10)
+Roll contains coins: -1, -3, -10, 0, 60
+Highest product: 60  (The coin is 60)
+Roll contains coins: -2, -40, 0, -2, -3
+Highest product: 80 (The coins are -2 and -40)
+
+Test Cases:
+
+a)
+Input
+ 
+5
+-2
+-3
+-5
+0
+-4
+ 
+Output
+15
+
+b)
+Input
+ 
+5
+-1
+-3
+-10
+60
+0
+ 
+Output
+1800
+            '''
+
+
+
 n = int(input())
 l = []
 for i in range(n):
