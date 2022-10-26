edc59ac925904644020d510b310b5a4f73bc76fb
nivanjana <nivanjana@qburst.com>
2022-10-26 17:32:23 +0530

    WIP p2 using stack

--- 2x.py
+++ 2x.py
@@ -25,40 +25,48 @@ o = []
 c = []
 temp = []
 i=0
-while i<len(a):
-    if a[i].isdigit():
-        k = int(a[i])
-        i+=1
-        while i<len(a) and s[i].isdigit():
-            k=k*10+int(a[i])
+try:
+    while i<len(a):
+        if a[i].isdigit():
+            k = int(a[i])
             i+=1
-        print(k)
-        c.append(k)
-    else:
-        if len(o)<=0:
-            o.append(a[i])
-        elif o[-1] in ('+','-') and a[i] in ['*','/','**','(']:
-            o.append(a[i])
-        elif a[i]==')':
-            while o[-1]!='(':
-                op = o.pop()
-                c1 = c.pop()
-                c2 = c.pop()
-                c.append(operation(c1,c2,op))
-            else:
-                o.pop()
+            while i<len(a) and a[i].isdigit():
+                k=k*10+int(a[i])
+                i+=1
+            c.append(k)
         else:
-            while (a[i] in ('+','-')) and (o[-1] in ['*','/','**','(']) and len(o)!=0:
-                temp.append(o.pop())
-            else:
+            if len(o)<=0:
                 o.append(a[i])
-            while len(temp)!=0:
-                o.append(temp.pop())
+            elif o[-1] in ('+','-') and a[i] in ['*','/','**','(']:
+                o.append(a[i])
+            elif a[i]==')':
+                while o[-1]!='(':
+                    op = o.pop()
+                    c1 = c.pop()
+                    c2 = c.pop()
+                    c.append(operation(c1,c2,op))
+                else:
+                    o.pop()
+            else:
+                while (a[i] in ('+','-')) and (o[-1] in ['*','/','**','(']) and len(o)!=0:
+                    temp.append(o.pop())
+                else:
+                    o.append(a[i])
+                while len(temp)!=0:
+                    o.append(temp.pop())
         i+=1
-if i>=len(a):
-      while len(o)!=0:
-        op = o.pop()
-        c1 = c.pop()
-        c2 = c.pop()
-        c.append(operation(c1,c2,op)) 
-print(c.pop())
+    if i>=len(a):
+        while len(o)!=0 and len(a)>=2:
+            op = o.pop()
+            c1 = c.pop()
+            c2 = c.pop()
+            c.append(operation(c1,c2,op))
+    if len(o)>0:
+        if o[-1] in ('+','-'):
+            print(str(o.pop())+str(c.pop()))
+        else:
+            print("Invalid Expression")
+    else:
+        print(c.pop())
+except IndexError:
+    print("Invalid Expression")
